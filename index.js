7001643c5e6c84db46f6538c55d237155d529635
Sergey Pepyakin <s.pepyakin@gmail.com>
2016-09-26 00:12:46 +0300

    /insert реализован

--- index.js
+++ index.js
@@ -16,30 +16,63 @@ try {
   db = hmsearch.openSync(DB_PATH, hmsearch.READWRITE);
 }
 
-app.get('/lookup', function (req, res, next) {
-  var image_url = req.query.image_url;
-  console.log("image_url=" + image_url);
-
+function get_image_blockhash(image_url, cb) {
   getPixels(image_url, function(pixels_err, pixels) {
     if (pixels_err) {
-      res.send(500);
+      cb(pixels_err, null);
     } else {
       var image_data = {
         width: pixels.shape[0],
         height: pixels.shape[1],
         data: pixels.data
       };
-      var image_hash = blockhash.blockhashData(image_data, 16, 1);
-      console.log("image_hash=" + image_hash);
+      try {
+        var image_hash = blockhash.blockhashData(image_data, 16, 1);
+        console.log("image_hash=" + image_hash);
+        cb(null, image_hash);
+      } catch (e) {
+        cb(e, null);
+      }
+    }
+  });
+}
+
+app.get('/lookup', function (req, res) {
+  var image_url = req.query.image_url;
+  console.log("lookup, image_url=" + image_url);
+
+  get_image_blockhash(image_url, function(err, image_hash) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
       db.lookup(image_hash, function(err, lookup_result) {
         if (err) {
           console.log("lookup error=" + err);
-          res.send(501);
+          res.sendStatus(501);
         } else {
           res.json({
             image_hash: image_hash,
             lookup_result: lookup_result
           });
+        }
+      });
+    }
+  });
+});
+
+app.put('/insert', function (req, res) {
+  var image_url = req.query.image_url;
+  console.log("insert, image_url=" + image_url);
+
+  get_image_blockhash(image_url, function(err, image_hash) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      db.insert(image_hash, function(err) {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
         }
       });
     }
